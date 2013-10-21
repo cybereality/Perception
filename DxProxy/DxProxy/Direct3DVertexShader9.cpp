bfe282b3f64d957cff32c60633348e10dcfeb5f2
EisernSchild <eisernschild@gmail.com>
2013-10-21 19:23:24 +0200

    Copyright Amendments

--- DxProxy/DxProxy/Direct3DVertexShader9.cpp
+++ DxProxy/DxProxy/Direct3DVertexShader9.cpp
@@ -1,8 +1,17 @@
 /********************************************************************
 Vireio Perception: Open-Source Stereoscopic 3D Driver
 Copyright (C) 2012 Andres Hernandez
-Modifications Copyright (C) 2013 Chris Drain, Denis Reischl, Neil Schneider 
-  and Joshua Brown 
+
+File <Direct3DVertexShader9.cpp> and
+Class <BaseDirect3DVertexShader9> :
+Copyright (C) 2013 Chris Drain
+
+Vireio Perception Version History:
+v1.0.0 2012 by Andres Hernandez
+v1.0.X 2013 by John Hicks, Neil Schneider
+v1.1.x 2013 by Primary Coding Author: Chris Drain
+Team Support: John Hicks, Phil Larkson, Neil Schneider
+v2.0.x 2013 by Denis Reischl, Neil Schneider, Joshua Brown
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
--- DxProxy/DxProxy/MotionTrackerFactory.cpp
+++ DxProxy/DxProxy/MotionTrackerFactory.cpp
@@ -2,6 +2,17 @@
 Vireio Perception: Open-Source Stereoscopic 3D Driver
 Copyright (C) 2012 Andres Hernandez
 
+File <MotionTrackerFactory.cpp> and
+Class <MotionTrackerFactory> :
+Copyright (C) 2012 Andres Hernandez
+
+Vireio Perception Version History:
+v1.0.0 2012 by Andres Hernandez
+v1.0.X 2013 by John Hicks, Neil Schneider
+v1.1.x 2013 by Primary Coding Author: Chris Drain
+Team Support: John Hicks, Phil Larkson, Neil Schneider
+v2.0.x 2013 by Denis Reischl, Neil Schneider, Joshua Brown
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
