0665e382b482b6184b5a52c43467bdcff3b47f05
Grant Bagwell <general@grantbagwell.co.uk>
2014-02-04 22:39:32 +0100

    Roll back

--- DxProxy/DxProxy/D3DProxyDevice.cpp
+++ DxProxy/DxProxy/D3DProxyDevice.cpp
@@ -2319,7 +2319,7 @@ void D3DProxyDevice::HandleControls()
 	if (hotkeyPressed)
 		menuVelocity.x+=2.0f;
 
-	// open BRASSA - <CTRL>+<T>
+	// open BRASSA - <CTRL>+<Q>
 	if(KEY_DOWN(0x51) && KEY_DOWN(VK_LCONTROL) && (menuVelocity == D3DXVECTOR2(0.0f, 0.0f)))
 	{
 		if (BRASSA_mode == BRASSA_Modes::INACTIVE)
