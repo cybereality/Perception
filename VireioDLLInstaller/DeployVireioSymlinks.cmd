2eae5f7ff4a5b57266258fd8242266a99bb61049
Simon <simonbrown77@googlemail.com>
2014-11-21 23:22:15 +0000

    Slight mod to the DLL copy batch scripts

--- VireioDLLInstaller/DeployVireioSymlinks.cmd
+++ VireioDLLInstaller/DeployVireioSymlinks.cmd
@@ -1,10 +1,3 @@
-@REM This isn't meant to be run by a double click, if it is, then it will start the installer app
-
-if "%1" EQU "" (
-	start VireioDLLInstaller
-	goto :EOF
-	)
-
 set CURDIR=%CD%
 
 mklink %1\d3d9.dll "%CURDIR%"\d3d9.dll
--- VireioDLLInstaller/RevokeVireioSymlinks.cmd
+++ VireioDLLInstaller/RevokeVireioSymlinks.cmd
@@ -1,10 +1,3 @@
-@REM This isn't meant to be run by a double click, if it is, then it will start the installer app
-
-if "%1" EQU "" (
-	start VireioDLLInstaller
-	goto :EOF
-	)
-
 del %1\d3d9.dll
 del %1\hijackdll.dll
 del %1\VRBoost.dll
