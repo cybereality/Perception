dbaa2b5160c2dbafbc764f97f4a192e3441e92a0
Simon <simonbrown77@googlemail.com>
2015-01-17 18:03:05 +0000

    Crysis - Complete
    VRBoost can now work for FOV addresses only, never going to figure out
    how Crytek stored orientation for Crysis, so VRBoost only affects FOVs.
    Seems to work quite well with mouse emulation anyway.

--- DxProxy/DxProxy/StereoView.cpp
+++ DxProxy/DxProxy/StereoView.cpp
@@ -130,7 +130,7 @@ StereoView::StereoView(ProxyHelper::ProxyConfig& config)
 		howToSaveRenderStates = HowToSaveRenderStates::STATE_BLOCK;
 		break;
 	case D3DProxyDevice::CRYENGINE:
-		howToSaveRenderStates = HowToSaveRenderStates::DO_NOT_SAVE_AND_RESTORE;
+		howToSaveRenderStates = HowToSaveRenderStates::ALL_STATES_MANUALLY;
 		break;
 	case D3DProxyDevice::GAMEBRYO:
 	case D3DProxyDevice::GAMEBRYO_SKYRIM:
