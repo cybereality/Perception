83eed342dd7534588973c474646d359ad3209bcf
Grant Bagwell <general@grantbagwell.co.uk>
2015-04-02 00:32:52 +0200

    Changes for DXHR
    State Block
    Menus

--- DxProxy/DxProxy/StereoView.cpp
+++ DxProxy/DxProxy/StereoView.cpp
@@ -143,7 +143,7 @@ StereoView::StereoView(ProxyHelper::ProxyConfig& config)
 		howToSaveRenderStates = HowToSaveRenderStates::STATE_BLOCK;
 		break;
 	case D3DProxyDevice::CDC:
-		howToSaveRenderStates = HowToSaveRenderStates::ALL_STATES_MANUALLY;
+		howToSaveRenderStates = HowToSaveRenderStates::STATE_BLOCK;
 		break;
 	case D3DProxyDevice::CHROME:
 		howToSaveRenderStates = HowToSaveRenderStates::ALL_STATES_MANUALLY;
